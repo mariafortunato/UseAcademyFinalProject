0abd6eb2913b70d658773972e128e60f0f221724
mariafortunato <malice.rf@hotmail.com>
2022-06-18 14:07:47 -0300

    layout tabBar

--- UsemobileProject/UsemobileProject/TabBar/TabBarViewController.swift
+++ UsemobileProject/UsemobileProject/TabBar/TabBarViewController.swift
@@ -12,18 +12,66 @@ class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setViewControllers([homeController(), addNewController(), favoritesController()], animated: true)
+        setupTabBar()
+
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupTabBar() {
+        //------------------- Todas as versões de iOS
+        
+        // Cor do icone e do texto da Tab selecionada
+        tabBar.tintColor = .white
+        
+        //------------------ Config versões anterior ao do iOS 15
+        
+        //Cor do icone e do texto da TabBar não selecionada
+        tabBar.unselectedItemTintColor = .black
+        
+        // Cor de fundo da TabBar
+        tabBar.isTranslucent = false
+        tabBar.barTintColor = .cyan
+        
+        //---------------- Config iOS 15 e posterior
+        
+        if #available(iOS 15, *){
+            let appearance = UITabBarAppearance()
+            
+            // Cor do texto das Tabs não selecionadas
+            appearance.stackedLayoutAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.black]
+            
+            // Cor dos icones das Tabs não selecionadas
+            appearance.stackedLayoutAppearance.normal.iconColor = .black
+            
+            // Cor de fundo da TabBar
+            appearance.configureWithOpaqueBackground()
+            appearance.backgroundColor = UIColor(red: 4.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1)
+            
+            UITabBar.appearance().standardAppearance = appearance
+            UITabBar.appearance().scrollEdgeAppearance = appearance
+            
+        }
     }
-    */
+    private func homeController() -> UIViewController {
+        let homeController = HomeViewController()
+        
+        homeController.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 0)
+        
+        return homeController
+    }
+    private func addNewController() -> UIViewController {
+        let addNewController = AddNewViewController()
+        
+        addNewController.tabBarItem = UITabBarItem(title: "Cadastrar", image: UIImage(systemName: "plus.circle"), tag: 0)
+        
+        return addNewController
+    }
+    
+    private func favoritesController() -> UIViewController {
+        let favoritesController = FavoritesViewController()
+        
+        favoritesController.tabBarItem = UITabBarItem(title: "Favoritos", image: UIImage(systemName: "star"), tag: 0)
+        
+        return favoritesController
+    }
 
 }
