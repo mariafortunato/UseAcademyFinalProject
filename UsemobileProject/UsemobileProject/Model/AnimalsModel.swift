88996cc8da897ee7f7d827a677d3ce6e92a78b72
Giselle Andrade <andrade.giselles@gmail.com>
2022-06-20 16:27:25 -0300

    Consumo API

--- UsemobileProject/UsemobileProject/Model/AnimalsModel.swift
+++ UsemobileProject/UsemobileProject/Model/AnimalsModel.swift
@@ -7,15 +7,34 @@
 
 import Foundation
 
-struct Animais: Decodable{
-        
-            var id: String?
-            var name: String?
-            var description: String?
-            var age: Int?
-            var species: String?
-            var image: String?
-            var created_at: String?
-            var updated_at: String?
+struct Animal: Decodable{
     
+    let items:[Items?]
+    
+}
+
+struct Items: Decodable{
+    
+    var id: String?
+    var name: String?
+    var description: String?
+    var age: Int?
+    var species: String?
+    var image: String?
+    var created_at: String?
+    var updated_at: String?
+    
+    init(){ }
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case name = "name"
+        case description = "description"
+        case age = "age"
+        case species = "species"
+        case image = "image"
+        case created_at = "crested_at"
+        case updated_at = "updated_at"
+    
+}
 }
